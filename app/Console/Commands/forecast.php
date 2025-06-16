eb52f7325715090e21cb7744a76504bf523cb0e5
lachlan <lachikent@gmail.com>
2025-06-16 15:51:52 +1000

    forecast init

---
+++ app/Console/Commands/forecast.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class forecast extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'forecast {cities?*}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Print the forecast for one or more cities for the next 5 days';
+
+    protected array $allowedCities = [
+        'Brisbane',
+        'Gold Coast',
+        'Sunshine Coast',
+    ];
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $cities = $this -> argument('cities');
+
+        if (empty($cities)) {
+            $this -> info("No cities provided.");
+            return;
+        }
+
+        $invalidCities = array_diff($cities, $this -> allowedCities);
+
+        if (!empty($invalidCities)) {
+            $this -> error('Invalid cities: ' . implode(', ', $invalidCities));
+            $this -> info('Allowed cities: ' . implode(', ', $this -> allowedCities));
+            return;
+        }
+
+        foreach ($cities as $city) {
+            $this -> line("Forecast for: $city");
+        }
+    }
+}
